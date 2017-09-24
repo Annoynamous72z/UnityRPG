fedfd9108044ca3ccc85d11ceff8d2fc820f9bb5
Annoynamous72z <ryanshi1999@gmail.com>
2017-09-24 00:01:01 -0400

    Added Stuff

---
+++ Assets/Scripts/FPSLimitScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSLimitScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		QualitySettings.vSyncCount = 0;
+		Application.targetFrameRate = 60;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
